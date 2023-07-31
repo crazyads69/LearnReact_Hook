579a92402215f24cb31e0564586549c2fd47ba56
crazyads69 <leminhtri11a1@gmail.com>
2023-07-31 20:41:36 +0700

    first commit

---
+++ src/PersonScore.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import { getPerson } from './getPerson';
+
+export function PersonScore() {
+    useEffect(() => {
+        async function getThePerson() {
+            const person = await getPerson();
+            console.log(person);
+        }
+        getThePerson();
+    }, []);
+    return null;
+}
